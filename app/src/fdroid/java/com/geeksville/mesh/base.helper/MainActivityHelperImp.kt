3d4a02940d86bf6aef0381109a2e09a322670638
Abdul Aziz Ansari <abduldblogger@gmail.com>
2022-01-24 14:55:08 +0530

    MainActivity refactored

---
+++ app/src/fdroid/java/com/geeksville/mesh/base.helper/MainActivityHelperImp.kt
@@ -0,0 +1,12 @@
+package com.geeksville.mesh.base.helper
+
+import android.content.Intent
+import com.geeksville.mesh.MainActivity
+
+class MainActivityHelperImp() : MainActivityHelper {
+    override fun askToRate(context: MainActivity) {
+    }
+
+    override fun googleSignIn(data: Intent?) {
+    }
+}
